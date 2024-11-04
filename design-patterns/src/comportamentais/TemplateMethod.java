e421d2c711a1774378b6c4f41be54e06a39ca81b
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-11-04 15:27:11 -0300

    teste

---
+++ design-patterns/src/comportamentais/TemplateMethod.java
@@ -0,0 +1,14 @@
+/*COMANDO:Estamos desenvolvendo um sistema bancário e precisamos modelar os
+diversos tipos de contas do banco. Toda operação bancária realizada
+gera a cobrança de uma taxa que é diferente para cada tipo da conta.*/
+
+package comportamentais;
+
+public class TemplateMethod {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
