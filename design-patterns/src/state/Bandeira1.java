84d1785b2b30514311564d29b86813e9288c8d42
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-11-10 21:35:05 -0300

    classe Bandeira1 implementa a interface Bandeira

---
+++ design-patterns/src/state/Bandeira1.java
@@ -0,0 +1,8 @@
+package state;
+
+public class Bandeira1 implements Bandeira {
+	public double calculaValorDaCorrida(double tempo, double distancia) {
+		return + tempo * 1.5 + distancia * 1.7;
+	}
+	
+}
