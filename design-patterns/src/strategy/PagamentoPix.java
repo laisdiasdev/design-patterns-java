5ee250386210809dd15f50f8d3cbd31fed8b90ad
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-11-10 22:54:50 -0300

    forma de pagamento 2

---
+++ design-patterns/src/strategy/PagamentoPix.java
@@ -0,0 +1,19 @@
+package strategy;
+
+public class PagamentoPix {
+	String chavePix;
+	
+	public PagamentoPix(String chavePix) {
+		this.chavePix = chavePix;
+	}
+
+	public void processarPagamento() {
+		conectarComBancoCentral();
+		System.out.println("Processando pagamento via PIX.");
+	}
+	
+	private void conectarComBancoCentral()
+	{
+		System.out.println("Buscando dados da chave PIX no Banco Central: " + chavePix);
+	}
+}
